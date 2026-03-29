6cc754ac6a6cd3331abed0115269f6cb3f33a124
achmadarifjailani <botolaqua907@gmail.com>
2026-03-29 15:56:25 +0700

    Update app.run to use environment port

--- app.py
+++ app.py
@@ -271,5 +271,7 @@ def generate_laporan():
     return send_file(filepath, as_attachment=True, download_name=filename)
 
 # ==================== JALANKAN APLIKASI ====================
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
